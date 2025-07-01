dbdf2a2672d3f8ad0146a2b2278c184f0128a540
ak24680 <kimihou0724@gmail.com>
2025-07-01 11:11:44 +0800

    作業

--- 0701/array_mode_count.java
+++ 0701/array_mode_count.java
@@ -24,3 +24,6 @@ public class array_mode_count {
     }
 }
 
+/*說明此作法的時間複雜度為 O(n²)，是否可改善？是否可改善?
+  可以
+  如果允許使用 Map（例如 HashMap<Integer, Integer>），只需要單次走訪（O(n)）就能統計每個元素的出現次數，然後再找最大值，整體時間複雜度為 O(n)*/
--- 0701/matrix_multiplication_trace.java
+++ 0701/matrix_multiplication_trace.java
@@ -1,36 +1,49 @@
-public class matrix_mul {
+public class matrix_multiplication_trace {
     public static void main(String[] args) {
         int[][] a = {
-            {1,2,3},
-            {4,5,6},
-            {7,8,9}
+            {1, 2},
+            {3, 4}
         };
-
         int[][] b = {
-            {1,2,3},
-            {4,5,6},
-            {7,8,9}
+            {5, 6},
+            {7, 8}
         };
 
-        int[][] c = new int [3][3];
+        int[][] c = new int[2][2];
 
-        for(int i = 0; i<3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.println("計算 c[" + i +"][" + j + "]:");
-                for (int k = 0; k < 3; k++) {
-                    int partial = a[i][k] * b[k][j];
-                    System.out.println(" a[" + i + "][" + k + "][" + j + "] = "
-                                       + a[i][k] + " * " + b[k][j] + " = " + partial);
-                    c[i][j] += partial;                   
-
+        for (int i = 0; i < 2; i++) {         
+            for (int j = 0; j < 2; j++) {     
+                int sum = 0;
+                StringBuilder trace = new StringBuilder();
+                trace.append("計算位置 c[" + i + "][" + j + "]：");
+                for (int k = 0; k < 2; k++) { 
+                    sum += a[i][k] * b[k][j];
+                    trace.append(a[i][k] + "*" + b[k][j]);
+                    if (k < 1) {
+                        trace.append(" + ");
+                    }
                 }
+                trace.append(" = " + (a[i][0] * b[0][j]) + " + " + (a[i][1] * b[1][j]));
+                trace.append(" = " + sum);
+                System.out.println(trace);
+                c[i][j] = sum;
             }
         }
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; i++) {
-                System.out.println(c[i][j] + "\t");
+        System.out.println("結果矩陣：");
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print(c[i][j] + " ");
             }
+            System.out.println();
         }
     }
 }
+
+/*若矩陣維度為 n × n，則：
+
+外層兩層 i、j 負責走訪結果矩陣的所有元素（n²）。
+
+內層 k 負責內積運算，需要 n 次乘法加總。
+
+總複雜度：O(n³)*/
